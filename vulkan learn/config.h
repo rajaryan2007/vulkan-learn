3eb2f5155929507d4b65500976831a0c4a2c81f1
rajaryan2007 <rajaryan149150@gmail.com>
2025-11-03 03:38:35 +0530

    image

--- vulkan learn/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vulkan/vulkan.h>
-#include <vector>
-
-const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
-};
--- vulkan learn/instance.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "helloTriangle.h"
-#include "config.h"
